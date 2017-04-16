47a014953955173628cc6adc0bdf11a4fda0e6f4
Mateus de Medeiros Cordeiro <mateusmcordeiro@hotmail.com>
2017-04-16 18:36:24 -0300

    Add files via upload

---
+++ lua/autorun/server/functions.lua
@@ -0,0 +1,110 @@
+include( "groupchange/config.lua" )
+
+--++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++--
+
+---------------------------------------------- FUNÇÕES MOTORAS -----------------------------------------
+
+--++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++--
+local function LothusZ.ArraySum(a1,a2)
+	for _,v in ipairs(a2) do 
+
+  	  table.insert(a1, v)
+
+	end
+end
+
+local function LothusZ.superadminSearch(id)
+	for i, v in ipairs(LothusZ.superadminList) do
+      if id == v then
+      	return true
+      end
+    end
+    return false
+end
+
+local function LothusZ.adminSearch(id)
+	for i, v in ipairs(LothusZ.adminList) do
+      if id == v then
+      	return true
+      end
+    end
+    return false
+end
+
+local function LothusZ.moderatorSearch(id)
+	for i, v in ipairs(LothusZ.moderatorList) do
+      if id == v then
+      	return true
+      end
+    end
+    return false
+end
+
+local function LothusZ.staffType(id)
+	if LothusZ.superadminSearch(id) then
+		return "superadmin"
+	elseif LothusZ.adminSearch(id) then
+		return "admin"
+	elseif LothusZ.moderatorSearch(id) then
+		return "moderator"
+	end
+end
+
+
+--++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++--
+
+
+
+--------------------------------------------------------------------------------------------------------
+
+------------------------------------------- FUNÇÕES PRINCIPAIS -----------------------------------------
+
+--------------------------------------------------------------------------------------------------------
+function LothusZ.hasID(id)
+	for i, v in ipairs(LothusZ.specialGroupIDS) do
+      if id == v then
+      	return true
+      end
+    end
+    return false
+end
+
+function LothusZ.specialGroupRemove(ply)
+	if(LothusZ.hasID(ply:SteamID()))then
+		RunConsoleCommand( "ulx","adduserid",ply:SteamID(),"vipdiamante" )
+	end
+end
+
+function LothusZ.jobChangedGroup(ply , oldjob , newjob)
+	if(LothusZ.hasID(ply:SteamID()))then
+		if(LothusZ.staffType(ply:SteamID()) == "superadmin") then
+			if newjob == DONO then
+				RunConsoleCommand( "ulx","adduserid",ply:SteamID(),"superadmin" )
+			else
+				LothusZ.specialGroupRemove(ply)
+			end
+		elseif LothusZ.staffType(ply:SteamID()) == "admin" then
+			if newjob == DONO then
+				RunConsoleCommand( "ulx","adduserid",ply:SteamID(),"admin" )
+			else
+				LothusZ.specialGroupRemove(ply)
+			end	
+		elseif LothusZ.staffType(ply:SteamID()) == "moderator" then
+			if newjob == DONO then
+				RunConsoleCommand( "ulx","adduserid",ply:SteamID(),"moderator" )
+			else
+				LothusZ.specialGroupRemove(ply)
+			end	
+		end
+	end
+end
+------------------------------------------------------------------------------------------------------
+
+
+LothusZ.ArraySum(LothusZ.specialGroupIDS,LothusZ.superadminList)
+LothusZ.ArraySum(LothusZ.specialGroupIDS,LothusZ.adminList)
+LothusZ.ArraySum(LothusZ.specialGroupIDS,LothusZ.moderatorList)
+
+
+hook.Add( "PlayerDisconnected", "remover_grupo_ao_sair" , LothusZ.specialGroupRemove )	
+hook.Add( "OnPlayerChangedTeam", "mudar_grupo" , LothusZ.jobChangedGroup )
---
+++ lua/groupchange/config.lua
@@ -0,0 +1,13 @@
+LothusZ = {}
+
+--lista de superadmins que vão querer ter a funcionalidade
+LothusZ.superadminList = {"STEAM_0:1:56730875","STEAM_0:0:38042265"}
+
+--lista de admins que vão querer ter a funcionalidade
+LothusZ.adminList = {"STEAM_0:1:XXXXXXXXX"}
+
+--lista de moderators que vão querer ter a funcionalidade
+LothusZ.moderatorList = {"STEAM_0:1:XXXXXXXXX"}
+
+--não modificar essa array
+LothusZ.specialGroupIDS = {"STEAM_0:1:XXXXXXXXX"}
