2cefaf09fc2096856ae04e52ad6b011fd6f662c6
Mateus de Medeiros Cordeiro <mateusmcordeiro@hotmail.com>
2017-04-16 19:07:28 -0300

    Update config.lua

--- lua/groupchange/config.lua
+++ lua/groupchange/config.lua
@@ -1,13 +1,13 @@
-LothusZ = {}
+GSC = {}
 
 --lista de superadmins que vão querer ter a funcionalidade
-LothusZ.superadminList = {"STEAM_0:1:56730875","STEAM_0:0:38042265"}
+GSC.superadminList = {"STEAM_0:1:56730875","STEAM_0:0:38042265"}
 
 --lista de admins que vão querer ter a funcionalidade
-LothusZ.adminList = {"STEAM_0:1:XXXXXXXXX"}
+GSC.adminList = {"STEAM_0:1:XXXXXXXXX"}
 
 --lista de moderators que vão querer ter a funcionalidade
-LothusZ.moderatorList = {"STEAM_0:1:XXXXXXXXX"}
+GSC.moderatorList = {"STEAM_0:1:XXXXXXXXX"}
 
 --não modificar essa array
-LothusZ.specialGroupIDS = {"STEAM_0:1:XXXXXXXXX"}
+GSC.specialGroupIDS = {"STEAM_0:1:XXXXXXXXX"}
